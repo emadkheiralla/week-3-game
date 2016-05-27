533cee13baef3d256dfb120e559a1cf9a4996b24
Emad Kheiralla <Emad Kheiralla>
2016-05-27 08:59:01 -0400

    Just about done with hangman game

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,26 @@
-
-
 	// Create an array that lists out all of the options
 	var cat = ['c','a','t'];
 	var bear = ['b','e','a','r'];
 	var tiger = ['t','i','g','e','r'];
 	var nextWord = ['cat', 'bear', 'tiger'];
-	var myArr = [];
+	var incorrectArr = [];
+	var uniqueIncorrectSet = new Set(incorrectArr);
+	var uniqueIncorrectArr = Array.from(uniqueIncorrectSet);
+	var correctArr = [];
+	var uniqueCorrectSet = new Set(correctArr);
+	var uniqueCorrectArr = Array.from(uniqueCorrectSet);
 	var myList = document.getElementById('letter');
 	var myListItem = "";
 	
-
 	// initialize counters
 	var wins = 0;
 	var guesses = 6;
+	var numLettersMatched = 0;
 
 	// Create code to randomly choose one of the three words  
 	var randomNumber = Math.random();
 	var optionsIndex = Math.floor(randomNumber * nextWord.length);
 	var currWord = nextWord[optionsIndex];
-	console.log(currWord);
 
 	document.querySelector('#numWin').innerHTML = wins;
 	document.querySelector('#numGuess').innerHTML = guesses;
@@ -28,9 +30,21 @@
 		myList.appendChild(myListItem);
 	}
 
+	function createCookie(name,value,days) {
+	    if (days) {
+	        var date = new Date();
+	        date.setTime(date.getTime()+(days*24*60*60*1000));
+	        var expires = "; expires="+date.toGMTString();
+	    }
+	    else var expires = "";
+	    document.cookie = name+"="+value+expires+"; path=/";
+	}
+
+
+
 	function reloadPage() {
 	    location.reload(true);
-	}	
+	}
 
 	function changeImage() {
 	    var image = document.querySelector("#hang");
@@ -57,89 +71,117 @@
 		// Determines which exact key was selected. Make it lowercase
 		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 
-		if(currWord.indexOf(userGuess) != -1 && guesses > 0){
-			if (currWord == 'cat' && userGuess == 'c') {
+		if(currWord.indexOf(userGuess) != -1 && guesses > 0) {
+			if (currWord == 'cat' && userGuess == 'c' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[0].innerHTML = 'c';
+				numLettersMatched++;
 			}
 
-			if (currWord == 'cat' && userGuess == 'a') {
+			if (currWord == 'cat' && userGuess == 'a' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[1].innerHTML = 'a';
+				numLettersMatched++;
 			}
 
-			if (currWord == 'cat' && userGuess == 't') {
+			if (currWord == 'cat' && userGuess == 't' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[2].innerHTML = 't';
+				numLettersMatched++;
 			}
 
 
-			if (currWord == 'bear' && userGuess == 'b') {
+			if (currWord == 'bear' && userGuess == 'b' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[0].innerHTML = 'b';
+				numLettersMatched++;
 			}
 
-			if (currWord == 'bear' && userGuess == 'e') {
+			if (currWord == 'bear' && userGuess == 'e' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[1].innerHTML = 'e';
+				numLettersMatched++;
 			}
 
-			if (currWord == 'bear' && userGuess == 'a') {
+			if (currWord == 'bear' && userGuess == 'a' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[2].innerHTML = 'a';
+				numLettersMatched++;
 			}
 
-			if (currWord == 'bear' && userGuess == 'r') {
+			if (currWord == 'bear' && userGuess == 'r' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[3].innerHTML = 'r';
+				numLettersMatched++;
 			}
 	
-			if (currWord == 'tiger' && userGuess == 't') {
+			if (currWord == 'tiger' && userGuess == 't' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[0].innerHTML = 't';
+				numLettersMatched++;
 			}
 
-			if (currWord == 'tiger' && userGuess == 'i') {
+			if (currWord == 'tiger' && userGuess == 'i' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[1].innerHTML = 'i';
+				numLettersMatched++;
 			}
 
-			if (currWord == 'tiger' && userGuess == 'g') {
+			if (currWord == 'tiger' && userGuess == 'g' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[2].innerHTML = 'g';
+				numLettersMatched++;
 			}
 
-			if (currWord == 'tiger' && userGuess == 'e') {
+			if (currWord == 'tiger' && userGuess == 'e' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[3].innerHTML = 'e';
+				numLettersMatched++;
 			}
 
-			if (currWord == 'tiger' && userGuess == 'r') {
+			if (currWord == 'tiger' && userGuess == 'r' && uniqueCorrectArr.indexOf(userGuess) == -1) {
+				uniqueCorrectArr += (userGuess);
 				document.querySelector('#yesno').innerHTML = 'You guessed right!';
 				document.getElementsByTagName('li')[4].innerHTML = 'r';
+				numLettersMatched++;
 			}
 
 			document.querySelector('#next').innerHTML = 'Choose another letter!';
 			document.querySelector('#hide').innerHTML = '';
+			
 
-		}else{
+		}else if(document.querySelector('#arrLetters').innerHTML == null || uniqueIncorrectArr.indexOf(userGuess) == -1){
+			uniqueIncorrectArr += (userGuess + ", ");
 			document.querySelector('#yesno').innerHTML = 'You guessed wrong!';
 			document.querySelector('#next').innerHTML = 'Choose another letter!';
 			document.querySelector('#hide').innerHTML = '';
-			document.querySelector('#arrLetters').innerHTML = myArr[userGuess];
-			changeImage();
+			document.querySelector('#arrLetters').innerHTML = uniqueIncorrectArr;
 			guesses--;
 			document.querySelector('#numGuess').innerHTML = guesses;
+			changeImage();
 		}
+
 		if (guesses == 0){
 			document.querySelector('#next').innerHTML = 'Game over, you lost!';
 			document.getElementById('submit').style.display = 'block';
 		}
 		
-		if (document.getElementsByTagName('li').innerHTML != null){
+		if(numLettersMatched == currWord.length){
 			document.querySelector('#next').innerHTML = 'Game over, you won!';
 			wins++;
 			document.querySelector('#numWin').innerHTML = wins;
+			document.getElementById('submit').style.display = 'block';
 		}
 		
 	};
